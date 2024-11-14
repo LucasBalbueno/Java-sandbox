be9df2774d101e889a2647c4b29e30d386fed6d4
LucasBalbueno <balbuenolucas04@gmail.com>
2024-11-14 18:01:33 -0300

    Criandot testes unitários

--- TestesUnitarios/src/main/java/javaSandbox/TestesUnitarios/DTO/ProductDTO.java
+++ TestesUnitarios/src/main/java/javaSandbox/TestesUnitarios/DTO/ProductDTO.java
@@ -9,4 +9,13 @@ public class ProductDTO {
     private String description;
     private float price;
     private Integer quantity;
+
+    public ProductDTO() {}
+
+    public ProductDTO(String name, String description, float price, Integer quantity) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.quantity = quantity;
+    }
 }
